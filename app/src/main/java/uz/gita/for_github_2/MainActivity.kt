2f994b93995d8392c24b50d8fcd22edd654694d2
DilmurodKhudoyberganov <khudouberganovdilmurod636@gmail.com>
2023-05-23 14:23:10 +0500

    qwwqdasd

--- app/src/main/java/uz/gita/for_github_2/MainActivity.kt
+++ app/src/main/java/uz/gita/for_github_2/MainActivity.kt
@@ -2,10 +2,12 @@ package uz.gita.for_github_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("asdf0","asd")
     }
 }
